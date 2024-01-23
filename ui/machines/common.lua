9782fc8fcf561bae2f8c434cc6714f8975473f98
Matthew W <fatboychummy@gmail.com>
2024-01-22 22:26:17 -0700

    Add MachineData class and disallow addition/removal of crafting table

--- ui/machines/common.lua
+++ ui/machines/common.lua
@@ -2,6 +2,10 @@
 
 local file_helper = require "file_helper" :instanced("data")
 
+---@class MachineData
+---@field name string The name of the machine.
+---@field preference_level number The preference level of the machine.
+
 local common = {
   machine_list = {}
 }
@@ -9,7 +13,7 @@ local common = {
 local SAVE_FILE = "machines.lson"
 
 --- Load the list of machine names from the file.
----@return table<string> list The list of machines.
+---@return table<string, MachineData> list The list of machines.
 function common.load()
   local list = file_helper:unserialize(SAVE_FILE, {})
   common.machine_list = list
@@ -18,25 +22,33 @@ end
 
 --- Save the list of machine names to the file.
 function common.save()
-  file_helper:write(SAVE_FILE, textutils.serialize(common.machine_list))
+  file_helper:serialize(SAVE_FILE, common.machine_list, true)
 end
 
 --- Add a machine to the list.
 ---@param name string The name of the machine to add.
 function common.add_machine(name)
-  common.remove_machine(name) -- ensure no duplicates are added.
-  table.insert(common.machine_list, name)
+  -- Disallow addition of crafting table.
+  if name == "Crafting Table" then
+    return
+  end
+
+  common.machine_list[name] {
+    name = name,
+    preference_level = 0
+  }
   common.save()
 end
 
 --- Remove a machine from the list.
 ---@param name string The name of the machine to remove.
 function common.remove_machine(name)
-  for i, v in ipairs(common.machine_list) do
-    if v == name then
-      table.remove(common.machine_list, i)
-    end
+  -- Disallow removal of crafting table.
+  if name == "Crafting Table" then
+    return
   end
+
+  common.machine_list[name] = nil
   common.save()
 end
 
