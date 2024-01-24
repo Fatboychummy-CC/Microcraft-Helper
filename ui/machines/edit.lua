21085af97df0f31e38b960d626ebfd94c6a7ba3a
Matthew W <fatboychummy@gmail.com>
2024-01-24 00:08:00 -0700

    Actual machine selection Now onto the next pages I guess

--- ui/machines/edit.lua
+++ ui/machines/edit.lua
@@ -4,8 +4,15 @@ local search = require "ui.util.search"
 --- Edit machine menu -> First, search for a machine by its name.
 ---@param run_menu fun(name: string) The function to run another menu
 return function(run_menu)
-  local machine = search("Microcraft Helper", "Select Machine", {"Iron Ingot", "Gold Ingot", "Lapis Lazuli", "Stick", "Diamond Axe", "Diamond", "Log", "Wooden Plank"})
+  local machine_names = {}
+  for name in pairs(common.machines) do
+    table.insert(machine_names, name)
+  end
+
+  local machine = search("Microcraft Helper", "Select Machine", machine_names)
   if machine then
-    -- Noot noot
+    print()
+    term.clear()
+    print(("Editing %s"):format(machine))
   end
 end
