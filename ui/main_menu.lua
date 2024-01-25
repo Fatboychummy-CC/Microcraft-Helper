c5d354fe10f72dd4bc1a8443497654cd4fec652c
Matthew W <fatboychummy@gmail.com>
2024-01-25 00:15:34 -0700

    A more fancy error catching system for the whole program

--- ui/main_menu.lua
+++ ui/main_menu.lua
@@ -1,9 +1,10 @@
 local quick_sub_menu = require "ui.quick_sub_menu"
+local catch_error = require "ui.util.catch_error"
 
 --- Main menu UI
 ---@param run_menu fun(name: string) The function to run another menu
 return function(run_menu)
-  quick_sub_menu("Main Menu", {
+  catch_error(quick_sub_menu, "Main Menu", {
     {
       name = "Items",
       description = "Add, remove, or edit items, as well as their recipes.",
