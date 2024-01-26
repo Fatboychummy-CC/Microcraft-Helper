755d97eead4594bfcf9b976e4719ffe1539e4468
Matthew W <fatboychummy@gmail.com>
2024-01-26 03:11:34 -0700

    Add util.get_line_count function to calculate the number of lines needed based on text width

--- lib/util.lua
+++ lib/util.lua
@@ -21,4 +21,22 @@ function util.deep_copy(t)
   return copy
 end
 
+--- Get the amount of lines output when given a specific amount of width.
+---@param text string The text to check.
+---@param width number The width to check.
+---@return number lines The amount of lines.
+function util.get_line_count(text, width)
+  local win = window.create(term.current(), 1, 1, width, 100, false)
+
+  local cx, cy = term.getCursorPos()
+  local old = term.redirect(win)
+
+  local lines = print(text)
+
+  term.redirect(old)
+  term.setCursorPos(cx, cy)
+
+  return lines or 0
+end
+
 return util
