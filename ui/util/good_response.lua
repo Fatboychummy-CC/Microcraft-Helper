7be82b2a05691c3a03c562058ea579933bf8e7e9
Matthew W <fatboychummy@gmail.com>
2024-02-07 00:18:18 -0700

    Refactor good_response.lua to allow customizable confirmation keys

--- ui/util/good_response.lua
+++ ui/util/good_response.lua
@@ -5,8 +5,11 @@ local util = require "util"
 --- Display a message to the user.
 ---@param subtitle string The subtitle of the menu.
 ---@param message string The message to display.
-return function(subtitle, message)
+---@param ... integer? The keys possible to use to confirm the selection. Defaults to just keys.enter.
+---@return integer key The key that was pressed to confirm the selection.
+return function(subtitle, message, ...)
   local w, h = term.getSize()
+  local keys_possible = table.pack(...)
 
   -- Set up the page.
   PrimeUI.clear()
@@ -16,7 +19,23 @@ return function(subtitle, message)
   PrimeUI.textBox(scrollbox, 1, 1, w - 5, 100, message, colors.blue)
   PrimeUI.textBox(term.current(), 3, h - 2, w - 4, 1, "Press enter to continue.", colors.white)
 
-  PrimeUI.keyAction(keys.enter, "done")
+  local key_pressed
+  if keys_possible.n == 0 then
+    keys_possible = {keys.enter, n = 1}
+  end
+
+  local function action(key)
+    return function()
+      key_pressed = key
+      PrimeUI.resolve("done")
+    end
+  end
+
+  for i = 1, keys_possible.n do
+    PrimeUI.keyAction(keys_possible[i], action(keys_possible[i]))
+  end
 
   PrimeUI.run()
+
+  return key_pressed
 end
