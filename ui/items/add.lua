2b581974a9b6ef0982eef84d2f439f648a1129e0
Matthew W <fatboychummy@gmail.com>
2024-03-07 00:38:00 -0700

    Fix outputting nil

--- ui/items/add.lua
+++ ui/items/add.lua
@@ -1,4 +1,5 @@
 local recipe_handler = require "recipe_handler"
+local items_common = require "ui.items.common"
 
 local get_item_details = require "ui.items.get_item_details"
 local catch_error = require "ui.util.catch_error"
@@ -12,6 +13,13 @@ return function(run_menu)
   if ok and recipe_data then
     recipe_handler.insert(recipe_data)
     recipe_handler.save()
-    good_response("Item added", ("Added item %s (outputs %d)."):format(recipe_data.result.name, recipe_data.result.amount))
+
+    local name = items_common.get_item_name(recipe_data.result.id)
+
+    if not name then
+      error(("Item name for item ID %d does not exist."):format(recipe_data.result.id), 0)
+    end
+
+    good_response("Item added", ("Added item %s (outputs %d)."):format(name, recipe_data.result.amount))
   end
 end
